a9b0dd7ab6d92fc8ff2a15a2479921e8764607ca
karthik_c <karthik_c@CHD118156.photonint.com>
2018-06-04 14:16:22 +0530

    Sample

---
+++ AlethiaFinanceService/src/main/java/com/alethia/AlethiaFinanceServiceApplication.java
@@ -0,0 +1,12 @@
+package com.alethia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlethiaFinanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlethiaFinanceServiceApplication.class, args);
+	}
+}
